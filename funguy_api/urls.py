3a36ac637ea670000d101f42758ee054ec6f89ea
Beck-R <Beckham.Reimer@gmail.com>
2022-03-30 19:38:18 -0500

    node endpoint finished, removed disks/partitions endpoints

--- funguy_api/urls.py
+++ funguy_api/urls.py
@@ -9,8 +9,6 @@ from rest_framework_nested import routers as nested_routers
 router = DefaultRouter()
 
 router.register(r'nodes', NodeViewSet, basename='node')
-router.register(r'disks', DiskViewSet)
-router.register(r'partitions', PartitionViewSet)
 router.register(r'brew', BrewViewSet, basename='brew')
 
 urlpatterns = [
